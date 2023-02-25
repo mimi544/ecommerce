21721a8b83807cd900ebcfc89ca5bac792763b81
mimi544 <59132699+mimi544@users.noreply.github.com>
2023-02-25 15:42:38 +0530

    decorating header

--- src/components/Helmet/Helmet.js
+++ src/components/Helmet/Helmet.js
@@ -1,9 +1,10 @@
 import React from 'react'
 
 const Helmet = (props) => {
+    document.title = 'Fast-Ecommerce -' + props.title
   return (
-    <div>
-      
+    <div className='w-100'>
+      {props.children}
     </div>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,36 @@
 import React from 'react';
 
+import Helmet from '../components/Helmet/Helmet';
+import { Container, Row, Col } from 'reactstrap';
+import heroImg from '../assets/images/hero-img.png'
+
 const Home = () => {
-    return <div>Home</div>;
+
+    const year = new Date ().getFullYear()
+    return <Helmet title={"Home"}>
+        <section className='hero_section'>
+            <Container>
+                <Row>
+                    <Col lg='6' md='6'>
+                        <div className='hero_content'>Trending Products In {year}<p className='hero_subtitle'></p>
+                        <h2>
+                            Make Your Interior More Minimalistic & Modern
+                        </h2>
+                        <p>Get your desireable interiors and make your home more beautiful with more fancy items</p>
+                        <button className='buy_btn'>SHOP NOW</button>
+                        </div>
+                    </Col>
+                    <Col lg='6' md='6'>
+                        <div className='hero_img'>
+                            <img src ={heroImg} alt =""/>
+
+                        </div>
+
+                    </Col>
+                </Row>
+            </Container>
+        </section>
+    </Helmet>
 };
 
 export default Home;
