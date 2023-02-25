3dc6f775df3545db7ef02c55e7fbb1d15fcf10cf
mimi544 <59132699+mimi544@users.noreply.github.com>
2023-02-25 14:35:01 +0530

    header icon decorated

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,12 +3,32 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import './header.css'
 
+import {motion} from 'framer-motion'
+
 import logo from '../../assets/images/logo.png'
 import userIcon from '../../assets/images/user-icon.png'
 
 
 
 import { Container, Row} from 'reactstrap';
+
+const nav_link = [
+    
+    {
+        path:'home',
+        display:'Home'
+    },
+    {
+        path:'shop',
+        display:'Shop'
+    },
+    {
+        path:'cart',
+        display:'Cart'
+    },
+
+]
+
 const Header = () =>{
     return <header className="header">
         <Container>
@@ -24,28 +44,20 @@ const Header = () =>{
 
                     <div className="navigation">
                         <ul className="menu">
-                            <li className="nav_item">
-                                <NavLink to ='home'>
-                                    Home
-                                </NavLink>
-                            </li>
-                            <li className="nav_item">
-                                <NavLink to ='home'>
-                                    Shop
-                                </NavLink>
-                            </li>
-                            <li className="nav_item">
-                                <NavLink to ='home'>
-                                    Cart
-                                </NavLink>
-                            </li>
+                            {
+                                nav_link.map((item,index)=>(
+                                    <li className='nav_item' key={index}><NavLink to={item.path} className= {(navClass)=>navClass.isActive ? 'nav_active':''}>{item.display}</NavLink></li>
+                                ))
+                            }
                         </ul>
                     </div>
 
                     <div className="nav_icons">
-                        <span className='fav_icon'><i className="ri-heart-3-fill"></i></span>
-                        <span className="cart_icon"><i className="ri-shopping-cart-2-fill"></i></span>
-                        <span><img src={userIcon} alt=''/></span>
+                        <span className='fav_icon'><i className="ri-heart-3-fill"></i><span className='badge'>1</span></span>
+                        <span className="cart_icon"><i className="ri-shopping-cart-2-fill"></i>
+                        <span className='badge'>1</span>
+                        </span>
+                        <span><motion.img whileTap ={{scale: 1.1}}src={userIcon} alt=''/></span>
                     </div>
                     <div className="mobile_menu">
                         <span><i className="ri-menu-fill"></i></span>
