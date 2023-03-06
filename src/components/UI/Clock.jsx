c39f321c5d4ee71029fe0f5953684653f76478a4
mimi544 <59132699+mimi544@users.noreply.github.com>
2023-03-06 20:45:38 +0530

    adding clock

--- src/components/UI/Clock.jsx
+++ src/components/UI/Clock.jsx
@@ -1,14 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Clock = () => {
+
+    const [days,setDays] = useState()
+    const [hours,setHours] = useState()
+    const [minutes,setMinutes] = useState()
+    const [seconds,setSeconds] = useState()
+
+    let interval;
+
+    const countDown = () =>{
+      const destination = new Date('March 10, 2023').getTime()
+
+      interval = setInterval(()=>{
+        const now = new Date().getTime()
+      })
+    }
+
   return (
-    <div className='clock_wrapper d-flex align-items-center gap-5'>
-      <div className='clock_data d-flex align-items-center gap-5'>
-        <div>
-            <h1 className='text-white fs-1'>10</h1>
-            <h5 className='text-white fs-5'>Days</h5>
+    <div className='clock_wrapper d-flex align-items-center gap-3'>
+      <div className='clock_data d-flex align-items-center gap-3'>
+        <div className='text-center'>
+            <h1 className='text-white fs-3 mb-2'>10</h1>
+            <h5 className='text-white fs-6'>Days</h5>
         </div>
-        <span className='text-white fs-1'>:</span>
+        <span className='text-white fs-3'>:</span>
+      </div>
+      <div className='clock_data d-flex align-items-center gap-3'>
+        <div className='text-center'>
+            <h1 className='text-white fs-3 mb-2'>10</h1>
+            <h5 className='text-white fs-6'>Hours</h5>
+        </div>
+        <span className='text-white fs-3'>:</span>
+      </div>
+      <div className='clock_data d-flex align-items-center gap-3'>
+        <div className='text-center'>
+            <h1 className='text-white fs-3 mb-2'>10</h1>
+            <h5 className='text-white fs-6'>Minutes</h5>
+        </div>
+        <span className='text-white fs-3'>:</span>
+      </div>
+      <div className='clock_data d-flex align-items-center gap-3'>
+        <div className='text-center'>
+            <h1 className='text-white fs-3 mb-2'>10</h1>
+            <h5 className='text-white fs-6'>Seconds</h5>
+        </div>
       </div>
     </div>
   )
