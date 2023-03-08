cbd5db29d890b0c6aa89d200a67b808794c374d7
mimi544 <59132699+mimi544@users.noreply.github.com>
2023-03-08 14:55:28 +0530

    footer added

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,15 +7,17 @@ import { Link } from 'react-router-dom';
 import logo from '../../assets/images/logo.png'
 
 const Footer = () =>{
+
+
+    const year = new Date().getFullYear()
     return (
         <footer className='footer'>
             <Container>
                 <Row>
                     <Col lg='4'>
                     <div className="logo">
-                            <img src={logo} alt='logo'/>
                             <div>
-                                <h1>Fast-Ecommerce</h1>
+                                <h1 className='text-white'>Fast-Ecommerce</h1>
                             </div>
                     </div>
                     <p className='footer_text mt-4'>
@@ -26,7 +28,7 @@ const Footer = () =>{
                     <Col lg='3'>
                     <div className='footer_quick-link'>
                             <h4 className='quick_links-title'>Top Categories</h4>
-                            <ListGroup className='mb-3'>
+                            <ListGroup >
                                 <ListGroupItem className='ps-0 border-0'>
                                     <Link to='#'>
                                         Mobile Phones
@@ -55,37 +57,67 @@ const Footer = () =>{
 
                     </Col>
 
-                    <Col lg='2'></Col>
+                    <Col lg='2'>
                     <div className='footer_quick-link'>
                             <h4 className='quick_links-title'>Useful Links</h4>
-                            <ListGroup className='mb-3'>
+                            <ListGroup>
                                 <ListGroupItem className='ps-0 border-0'>
-                                    <Link to='#'>
-                                        Mobile Phones
+                                    <Link to='/shop'>
+                                        Shop
+                                    </Link>
+                                </ListGroupItem>
+
+                                <ListGroupItem className='ps-0 border-0'>
+                                    <Link to='/cart'>
+                                        Cart
+                                    </Link>
+                                </ListGroupItem>
+
+                                <ListGroupItem className='ps-0 border-0'>
+                                    <Link to='/login'>
+                                        Login
                                     </Link>
                                 </ListGroupItem>
 
                                 <ListGroupItem className='ps-0 border-0'>
                                     <Link to='#'>
-                                        Modern Sofa
-                                    </Link>
-                                </ListGroupItem>
-
-                                <ListGroupItem className='ps-0 border-0'>
-                                    <Link to='#'>
-                                        Arm Chair
-                                    </Link>
-                                </ListGroupItem>
-
-                                <ListGroupItem className='ps-0 border-0'>
-                                    <Link to='#'>
-                                        Smart Watches
+                                        Privacy Policies
                                     </Link>
                                 </ListGroupItem>
                             </ListGroup>
                         </div>
+                    </Col>
+                    
+                    <Col lg='3'>
+                    <div className='footer_quick-link'>
+                            <h4 className='quick_links-title'>Contact</h4>
+                            <ListGroup>
+                                <ListGroupItem className='ps-0 border-0'>
+                                    <span><i className="ri-map-pin-user-line"></i></span>
+                                    <p>123, Natunpullay , Near Sagar Sethu Club , Howrah - 711113</p>
+                                </ListGroupItem>
 
-                    <Col lg='3'></Col>
+                                <ListGroupItem className='ps-0 border-0'>
+                                    <span><i className="ri-phone-fill"></i></span>
+                                    <p>+918240150887</p>
+                                </ListGroupItem>
+
+                                <ListGroupItem className='ps-0 border-0'>
+                                    <span><i className="ri-mail-open-fill"></i></span>
+                                    <p>ssohini50@gmail.com</p>
+                                </ListGroupItem>
+
+                                <ListGroupItem className='ps-0 border-0'>
+                                    <Link to='#'>
+                                        Privacy Policies
+                                    </Link>
+                                </ListGroupItem>
+                            </ListGroup>
+                        </div>
+                    </Col>
+                    <Col lg='12'>
+                        <p className='footer_copyright'>Copyright {year} developed by Sohini Sen.All Rights are reserved.</p>
+                    </Col>
                 </Row>
             </Container>
         </footer>
