1fce82324a4d005367f67ec108a9d7f800920142
mimi544 <59132699+mimi544@users.noreply.github.com>
2023-04-25 16:54:01 +0530

    adding sections in shop page

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,10 +1,85 @@
-import React from "react";
+import React, { useState } from "react";
 import CommonSection from "../components/UI/CommonSection";
 import Helmet from "../components/Helmet/Helmet";
+import { Container, Row, Col } from "reactstrap";
+import "../styles/shop.css";
+import products from "../assets/data/products";
+import ProductsList from "../components/UI/ProductsList";
+
 const Shop = () => {
+	const [productsData, setProductsData] = useState(products);
+	const handleFilter = (e) => {
+		const filterValue = e.target.value;
+		if (filterValue === "sofa") {
+			const filteredProducts = products.filter(
+				(item) => item.category === "sofa"
+			);
+			setProductsData(filteredProducts);
+		}
+	};
+
 	return (
 		<Helmet title="Shop">
 			<CommonSection title="Products" />{" "}
+			<section>
+				<Container>
+					<Row>
+						<Col lg="3" md="3">
+							<div className="filter_widget">
+								{" "}
+								<select>
+									<option> Filter By Category </option>{" "}
+									<option value="sofa"> Sofa </option>{" "}
+									<option value="mobile"> Mobile </option>{" "}
+									<option value="chair"> Chair </option>{" "}
+									<option value="watch"> Watch </option>{" "}
+									<option value="wireless"> Wireless </option>{" "}
+								</select>{" "}
+							</div>{" "}
+						</Col>{" "}
+						<Col lg="3" md="3">
+							<div className="filter_widget">
+								{" "}
+								<select onChange={handleFilter}>
+									<option> Sort By Category </option>{" "}
+									<option value="ascending">
+										{" "}
+										Ascending{" "}
+									</option>{" "}
+									<option value="descending">
+										{" "}
+										Descending{" "}
+									</option>{" "}
+								</select>{" "}
+							</div>{" "}
+						</Col>{" "}
+						<Col lg="6" md="6">
+							<div className="search_box">
+								<input
+									type="text"
+									placeholder="Search here..."
+								/>{" "}
+								<span>
+									{" "}
+									<i class="ri-search-eye-line"> </i>{" "}
+								</span>{" "}
+							</div>{" "}
+						</Col>{" "}
+					</Row>{" "}
+				</Container>{" "}
+			</section>{" "}
+			<section>
+				<Container>
+					<Row>
+						{" "}
+						{productsData.length === 0 ? (
+							<h1> No Product Found </h1>
+						) : (
+							<ProductsList data={productsData} />
+						)}{" "}
+					</Row>{" "}
+				</Container>{" "}
+			</section>{" "}
 		</Helmet>
 	);
 };
