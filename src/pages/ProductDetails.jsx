cfcb23f1ae2a69235f0d6e377950025d2056495d
mimi544 <59132699+mimi544@users.noreply.github.com>
2023-04-26 18:24:12 +0530

    styling rating

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -4,12 +4,20 @@ import { useParams } from "react-router-dom";
 import products from "../assets/data/products";
 import CommonSection from "../components/UI/CommonSection";
 import Helmet from "../components/Helmet/Helmet";
+import "../styles/product-details.css";
 
 const ProductDetails = () => {
 	const { id } = useParams();
 	const product = products.find((item) => item.id === id);
-	const { imgUrl, productName, price, avgRating, review, description } =
-		product;
+	const {
+		imgUrl,
+		productName,
+		price,
+		avgRating,
+		review,
+		description,
+		shortDesc,
+	} = product;
 
 	return (
 		<Helmet>
@@ -25,8 +33,8 @@ const ProductDetails = () => {
 							{" "}
 							<div className="product_details">
 								{" "}
-								<h2> Product Name </h2>{" "}
-								<div className="product_rating">
+								<h2> {productName} </h2>{" "}
+								<div className="product_rating d-flex align-items-center gap-5 mb-3">
 									<div>
 										{" "}
 										<span>
@@ -66,6 +74,11 @@ const ProductDetails = () => {
 										ratings){" "}
 									</p>{" "}
 								</div>{" "}
+								<span> {price} </span> <p> {shortDesc} </p>{" "}
+								<button className="buy_btn">
+									{" "}
+									Add to Cart{" "}
+								</button>{" "}
 							</div>{" "}
 						</Col>{" "}
 					</Row>{" "}
