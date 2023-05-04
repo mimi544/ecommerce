c245c840d9191b427415ab4109b4ba4013bd81fd
mimi544 <59132699+mimi544@users.noreply.github.com>
2023-05-04 14:20:20 +0530

    adding options in cart page

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -3,13 +3,14 @@ import "../styles/cart.css";
 import Helmet from "../components/Helmet/Helmet";
 import CommonSection from "../components/UI/CommonSection";
 import { Container, Row, Col } from "reactstrap";
-import tdImg from "../assets/images/arm-chair-01.jpg";
 import { motion } from "framer-motion";
 import { cartActions } from "../redux/slices/cartSlice";
 import { useSelector, useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
 
 const Cart = () => {
 	const cartItems = useSelector((state) => state.cart.cartItems);
+	const totalAmount = useSelector((state) => state.cart.totalAmount);
 	return (
 		<Helmet title="Cart">
 			<CommonSection title="Shopping Cart" />
@@ -37,39 +38,80 @@ const Cart = () => {
 									<tbody>
 										{" "}
 										{cartItems.map((item, index) => (
-											<tr>
-												{" "}
-												<td>
-													{" "}
-													<img
-														src={tdImg}
-														alt=""
-													/>{" "}
-												</td>{" "}
-												<td> {item.productName} </td>{" "}
-												<td> {item.price} </td>{" "}
-												<td>
-													{" "}
-													{item.quantity}
-													px{" "}
-												</td>{" "}
-												<td>
-													{" "}
-													<i className="ri-delete-bin-6-line">
-														{" "}
-													</i>{" "}
-												</td>{" "}
-											</tr>
+											<Tr item={item} key={index} />
 										))}{" "}
 									</tbody>{" "}
 								</table>
 							)}{" "}
 						</Col>{" "}
-						<Col lg="3"> </Col>{" "}
+						<Col lg="3">
+							{" "}
+							<div>
+								<h6 className="d-flex align-items-center justify-content between">
+									{" "}
+									Subtotal{" "}
+									<span className="fs-4 fw-bold">
+										{" "}
+										{totalAmount}{" "}
+									</span>{" "}
+								</h6>{" "}
+							</div>{" "}
+							<p className="fs-6 mt-2">
+								{" "}
+								taxes and shipping will calculate in checkout{" "}
+							</p>{" "}
+							<div>
+								{" "}
+								<button className="buy_btn w-100 ">
+									{" "}
+									<Link to="/checkout"> Checkout </Link>{" "}
+								</button>{" "}
+								<button className="buy_btn w-100 mt-3">
+									{" "}
+									<Link to="/shop">
+										{" "}
+										Continue Shopping{" "}
+									</Link>{" "}
+								</button>{" "}
+							</div>{" "}
+						</Col>{" "}
 					</Row>{" "}
 				</Container>{" "}
 			</section>{" "}
 		</Helmet>
+	);
+};
+const Tr = ({ item }) => {
+	const dispatch = useDispatch();
+	const deleteProduct = () => {
+		dispatch(cartActions.deleteItem(item.id));
+	};
+	return (
+		<tr>
+			{" "}
+			<td>
+				{" "}
+				<img src={item.imgUrl} alt="" />{" "}
+			</td>{" "}
+			<td> {item.productName} </td> <td> {item.price} </td>{" "}
+			<td>
+				{" "}
+				{item.quantity}
+				px{" "}
+			</td>{" "}
+			<td>
+				{" "}
+				<motion.i
+					whileTap={{
+						scale: 1.2,
+					}}
+					onClick={deleteProduct}
+					className="ri-delete-bin-6-line"
+				>
+					{" "}
+				</motion.i>{" "}
+			</td>{" "}
+		</tr>
 	);
 };
 
