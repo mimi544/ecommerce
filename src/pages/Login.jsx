a2d1c586a321323c48fd645849f0d6d4294ceb65
mimi544 <59132699+mimi544@users.noreply.github.com>
2023-05-06 12:39:50 +0530

    designing the signup page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,29 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import Helmet from "../components/Helmet/Helmet";
 import { Container, Row, Col, Form, FormGroup } from "reactstrap";
 import { Link } from "react-router-dom";
 import "../styles/login.css";
 
-function Login() {
+const Login = () => {
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+
 	return (
 		<Helmet title="Login">
 			<section>
 				<Container>
 					<Row>
 						<Col lg="6" className="m-auto text-center">
-							<h3 className="fw-bold fs-4"> Login </h3>{" "}
+							<h3 className="fw-bold mb-4"> Login </h3>{" "}
 							<Form className="auth_form">
 								<FormGroup className="form_group">
 									<input
 										type="email"
 										placeholder="Enter your email"
-									/>
+										value={email}
+										onChange={(e) =>
+											setEmail(e.target.value)
+										}
+									/>{" "}
 									<input
 										type="password"
 										placeholder="Enter your password"
-									/>
+										value={password}
+										onChange={(e) =>
+											setPassword(e.target.value)
+										}
+									/>{" "}
 								</FormGroup>{" "}
-								<button className="buy_btn auth_btn">
+								<button
+									type="submit"
+									className="buy_btn auth_btn"
+								>
 									{" "}
 									Login{" "}
 								</button>{" "}
@@ -42,6 +56,6 @@ function Login() {
 			</section>{" "}
 		</Helmet>
 	);
-}
+};
 
 export default Login;
