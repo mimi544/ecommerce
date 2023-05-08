39d071b800efe6b1333c6ebbeb2eef8df5b87e33
mimi544 <59132699+mimi544@users.noreply.github.com>
2023-05-08 10:03:33 +0530

    done the login page

---
+++ src/custom-hooks/useAuth.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const useAuth = () => {
+  return (
+    <div>
+      Auth
+    </div>
+  )
+}
+
+export default useAuth
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,21 +1,47 @@
 import React, { useState } from "react";
 import Helmet from "../components/Helmet/Helmet";
 import { Container, Row, Col, Form, FormGroup } from "reactstrap";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import {signInWithEmailAndPassword } from "firebase/auth"
+import { auth } from "../firebase.config";
 import "../styles/login.css";
+import {toast} from "react-toastify"
 
 const Login = () => {
 	const [email, setEmail] = useState("");
 	const [password, setPassword] = useState("");
+	const [loading,setLoading] = useState(false)
+	const navigate = useNavigate();
+
+	const signIn = async (e)=>{
+		e.preventDefault()
+				setLoading(true)
+				try{
+					const userCredential = await signInWithEmailAndPassword(auth,email,password).catch(err => {console.error(err.message);});
+					const user = userCredential.user
+					console.log(user)
+					setLoading(false)
+					toast.success("Login Successful")
+					navigate ("/checkout")
+					
+				}
+				catch(error){
+					setLoading(false)
+					toast.error(error.message)
+				}
+				setLoading(false)
+	}
 
 	return (
 		<Helmet title="Login">
 			<section>
 				<Container>
 					<Row>
-						<Col lg="6" className="m-auto text-center">
+						{
+							loading ? (<Col lg = "12" className="text-center">
+								<h5 className="fw-bold">loading......</h5></Col>):(<Col lg="6" className="m-auto text-center">
 							<h3 className="fw-bold mb-4"> Login </h3>{" "}
-							<Form className="auth_form">
+							<Form className="auth_form" onSubmit={signIn}>
 								<FormGroup className="form_group">
 									<input
 										type="email"
@@ -50,8 +76,10 @@ const Login = () => {
 									</Link>{" "}
 								</p>{" "}
 							</Form>{" "}
-						</Col>{" "}
-					</Row>{" "}
+						</Col>
+								)
+						}
+					</Row>
 				</Container>{" "}
 			</section>{" "}
 		</Helmet>
